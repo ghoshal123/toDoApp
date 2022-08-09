08c110caf85b8c30dd0f01957007894fe208e7d3
Rahul Ghoshal <107094884+ghoshal123@users.noreply.github.com>
2022-08-09 10:07:02 +0530

    Update TodoList.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -17,7 +17,7 @@ const TodoList = ({ element, notes, setTitle,  setNotes,  title }) => {
     }
   return (
     <div>
-    <div className="card-header" style={{ color: "black" }}><h3>Todos:{element.todo}</h3></div>
+    <div className="card-header" style={{ color: "black",textDecoration: line ? 'line-through' : "none" }}><h3>Todos:{element.todo}</h3></div>
             <div className="card-body text-secondary">
                 <button type="button" className="btn btn-Light mx-2" onClick={() => { removeData(element.id) }}><i class="icon-trash" style={{fontSize:"40px",color:"red"}}></i></button>
                 <button type="button" className="btn btn-Light mx-2" onClick={completeData}><i class="icon-check" style={{fontSize:"40px",color:"green"}}></i></button>
