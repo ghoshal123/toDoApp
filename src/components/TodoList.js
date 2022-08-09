509846e3a62b78a22ea44825a2483d9e1064387f
Rahul Ghoshal <107094884+ghoshal123@users.noreply.github.com>
2022-08-09 10:05:43 +0530

    Update TodoList.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
 const TodoList = ({ element, notes, setTitle,  setNotes,  title }) => {
+    const [line, setLine] = useState(false);
+    
     const removeData = (id) => {
         const newNotes = notes.filter((elm) => {
             if (elm.id !== id) {
@@ -10,15 +12,15 @@ const TodoList = ({ element, notes, setTitle,  setNotes,  title }) => {
         setNotes(newNotes);
     }
 
-    const completeData=(id)=>{
-        alert("Task completed");
+    const completeData=()=>{
+        setLine(true);
     }
   return (
     <div>
     <div className="card-header" style={{ color: "black" }}><h3>Todos:{element.todo}</h3></div>
             <div className="card-body text-secondary">
                 <button type="button" className="btn btn-Light mx-2" onClick={() => { removeData(element.id) }}><i class="icon-trash" style={{fontSize:"40px",color:"red"}}></i></button>
-                <button type="button" className="btn btn-Light mx-2" onClick={() => { completeData(element.id) }}><i class="icon-check" style={{fontSize:"40px",color:"green"}}></i></button>
+                <button type="button" className="btn btn-Light mx-2" onClick={completeData}><i class="icon-check" style={{fontSize:"40px",color:"green"}}></i></button>
             </div>
 
     </div>
